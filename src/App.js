4e6562887dd82d876f30a65736a3fc0927c2b06e
NEWUSER <chasevriend84@gmail.com>
2023-04-24 08:25:11 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import TinderMatches from './TinderMatches';
 import Chats from './Chats';
 import ChatScreen from './ChatScreen';
 import './App.css';
-
 
 function App() {
   return (
